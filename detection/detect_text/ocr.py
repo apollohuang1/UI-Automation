b14f81e19a188a67f8fb4f91579a7230b5d48915
MulongXie <dsh15325@163.com>
2023-02-08 15:47:47 +1100

    update ocr apikey

--- detection/detect_text/ocr.py
+++ detection/detect_text/ocr.py
@@ -25,7 +25,7 @@ def Google_OCR_makeImageData(imgpath):
 def ocr_detection_google(imgpath):
     start = time.clock()
     url = 'https://vision.googleapis.com/v1/images:annotate'
-    api_key = 'AIzaSyDUc4iOUASJQYkVwSomIArTKhE2C6bHK8U'             # *** Replace with your own Key ***
+    api_key = 'AIzaSyDVQQZpxdFBKc-lpWb57vnbSjFNG2YRrqI'             # *** Replace with your own Key ***
     imgdata = Google_OCR_makeImageData(imgpath)
     response = requests.post(url,
                              data=imgdata,
@@ -33,6 +33,8 @@ def ocr_detection_google(imgpath):
                              headers={'Content_Type': 'application/json'})
     # print('*** Text Detection Time Taken:%.3fs ***' % (time.clock() - start))
     print("*** Please replace the Google OCR key at detect_text/ocr.py line 28 with your own (apply in https://cloud.google.com/vision) ***")
+    if 'responses' not in response.json():
+        raise Exception(response.json())
     if response.json()['responses'] == [{}]:
         # No Text
         return None
