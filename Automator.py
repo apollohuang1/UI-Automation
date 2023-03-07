5e999830616ae3e421b55914dde0a118f41d1d91
MulongXie <dsh15325@163.com>
2023-03-07 20:37:42 +1100

    add generation description for blocks

--- Automator.py
+++ Automator.py
@@ -7,60 +7,75 @@ class Automator:
         self.element_desc = []
 
         openai.api_key = open('openaikey.txt', 'r').readline()
-        self.task = task            # string, a NL sentence to describe the task, e.g. "Turn on voice"
-        self.prompt = None          # string, prompt including the task and textual_element as options sent to the LLM
-        self.openai_responses = []  # the response from openai asked the prompt
-        self.openai_answers = []    # the text answer in the openai_resp
+        self.role = 'You are a mobile virtual assistant that understands and interacts with the user interface to complete given task.'
+        self.conversation = [{'role': 'system', 'content': self.role}]    # store the conversation history [{'role':, 'content':}]
+        self.task = task                # string, a NL sentence to describe the task, e.g. "Turn on voice"
 
-    def gather_element_descriptions(self):
-        for ele in self.gui.elements_leaves:
-            if ele['description']:
-                self.element_desc.append((ele['leaf-id'], ele['description']))
+        self.block_description = []     # list of strings describing block
+        self.block_identification = ''  # answer for block_identification
 
     def show_target_element(self, ele_id):
         print(self.gui.elements_leaves[ele_id])
         self.gui.show_element(self.gui.elements_leaves[ele_id])
 
     '''
+    ***************
+    *** AI Chain***
+    ***************
+    '''
+    def generate_descriptions_for_blocks(self):
+        prompt = 'This is a code snippet that descript a part of UI, summarize its functionalities in one paragraph.\n'
+        for block in self.gui.blocks:
+            self.block_description.append(self.ask_openai(prompt + str(block)))
+
+    def target_block_identification(self):
+        prompt = 'I will give you a list of blocks in the UI, is any of them related to the task "' + self.task + '"? ' \
+                 'If yes, which block is the most related to complete the task?\n'
+        for i, block_desc in enumerate(self.block_description):
+            prompt += '[Block ' + str(i) + ':' + block_desc + '\n'
+        prompt += '\n Just ask [Yes, Block id] if any block is related or [No] if not.'
+        self.block_identification = self.ask_openai(prompt)
+
+    # def scrollable_block_check(self):
+
+    '''
     ******************
     *** OpenAI LLM ***
     ******************
     '''
-    def assemble_prompt(self):
-        pre_prompt = 'You have a list of UI components on the current UI page, which one of them is more related to the task "' + self.task + '"?\n'
-        post_prompt = 'Components:['
-        for desc in self.element_desc:
-            post_prompt += str(desc) + ';'
-        self.prompt = pre_prompt + post_prompt + ']\n'
-        self.prompt += 'Note that it may take multiple steps to reach the target UI with the target component to complete the task. ' \
-                       'If no component in the current UI that directly related to the task, which of them is more possible to lead to the target UI?'
-
-    def select_element_to_perform_task(self, task):
-        self.task = task
-        self.assemble_prompt()
-        self.ask_openai(self.prompt)
-
-    def ask_openai(self, prompt):
-        print('*** Asking ***\n', prompt)
+    def ask_openai_prompt(self, prompt, role=None):
+        if not role: role = self.role
+        print('*** Asking ***\n', role, '\n', prompt)
         resp = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=[
-                    {'role': 'system', 'content': 'You are a mobile virtual assistant that understands and interacts with the user interface to complete given task.'},
+                    {'role': 'system', 'content': self.role},
                     {'role': 'user', 'content': prompt}
                 ]
             )
-        self.openai_responses.append(resp)
-        self.openai_answers.append(resp['choices'][0].message)
         print('\n*** Answer ***\n', resp['choices'][0].message)
+        return resp['choices'][0].message
+
+    def ask_openai_conversation(self, conversation):
+        if not conversation: conversation = self.conversation
+        print('*** Asking ***\n', conversation)
+        resp = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    conversation
+                ]
+            )
+        print('\n*** Answer ***\n', resp['choices'][0].message)
+        return resp['choices'][0].message
 
 
 if __name__ == '__main__':
     from GUIData import GUIData
     gui = GUIData('data/emulator-5554.png', 'data/emulator-5554.json')
-    gui.extract_elements_from_vh()
-    gui.show_all_elements()
+    gui.ui_info_extraction()
+    gui.ui_analysis_elements_description()
+    gui.ui_element_block_tree()
     gui.show_all_elements(only_leaves=True)
-    gui.extract_elements_description()
 
     aut = Automator(gui)
     aut.gather_element_descriptions()
