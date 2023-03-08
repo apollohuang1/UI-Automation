a3f7dc6cb20b4948d3c6d5eabcf68eca43c2ae28
MulongXie <dsh15325@163.com>
2023-03-08 15:47:28 +1100

    record conversation for element and block ai chain respectively

--- Automator.py
+++ Automator.py
@@ -1,16 +1,22 @@
 import openai
 import json
+from os.path import join as pjoin
 
 
 class Automator:
     def __init__(self, gui=None, task=''):
-        self.output_root = 'data/openai'
         self.gui = gui
         self.element_desc = []
 
+        self.output_root = 'data/openai'
+        self.output_block_desc = pjoin(self.output_root, 'block_desc.json')
+        self.output_chain_block = pjoin(self.output_root, 'chain_block.json')
+        self.output_chain_element = pjoin(self.output_root, 'chain_element.json')
+
         openai.api_key = open('openaikey.txt', 'r').readline()
         self.role = 'You are a mobile virtual assistant that understands and interacts with the user interface to complete given task.'
-        self.conversation = [{'role': 'system', 'content': self.role}]    # store the conversation history [{'role':, 'content':}]
+        self.chain_block = [{'role': 'system', 'content': self.role}]       # store the conversation history for block ai chain
+        self.chain_element = [{'role': 'system', 'content': self.role}]     # store the conversation history for element ai chain
         self.task = task                # string, a NL sentence to describe the task, e.g. "Turn on voice"
 
         self.block_descriptions = []        # list of strings describing block
@@ -44,26 +50,26 @@ class Automator:
         for i, block_desc in enumerate(self.block_descriptions):
             prompt += '[Block ' + str(i) + ']:' + block_desc + '\n'
         prompt += '\n Answer [Yes] with the most related block if any or [No] if not.'
-        self.conversation = [{'role': 'system', 'content': self.role}]
-        self.conversation.append({'role': 'user', 'content': prompt})
-        self.conversation.append(self.ask_openai_conversation())
-        self.block_identification = self.conversation[-1]['content']
+        self.chain_block = [{'role': 'system', 'content': self.role}]
+        self.chain_block.append({'role': 'user', 'content': prompt})
+        self.chain_block.append(self.ask_openai_conversation(self.chain_block))
+        self.block_identification = self.chain_block[-1]['content']
 
     def scrollable_block_check(self):
         prompt = {'role': 'user',
                   'content': 'For scrollable blocks, is it possible that the UI elements related to the task would show up after scroll? '
                              'Answer [Yes] with the most related block if any or [No] if not'}
-        self.conversation.append(prompt)
-        self.conversation.append(self.ask_openai_conversation())
-        self.block_scrollable_check = self.conversation[-1]['content']
+        self.chain_block.append(prompt)
+        self.chain_block.append(self.ask_openai_conversation(self.chain_block))
+        self.block_scrollable_check = self.chain_block[-1]['content']
 
     def intermediate_block_check(self):
         prompt = {'role': 'user',
                   'content': 'The task may take multiple steps to complete, is there any block likely to jump to the UI that is related to the task? ' +
                              'Answer [Yes] with the most related block if any or [No] if not'}
-        self.conversation.append(prompt)
-        self.conversation.append(self.ask_openai_conversation())
-        self.block_intermediate_check = self.conversation[-1]['content']
+        self.chain_block.append(prompt)
+        self.chain_block.append(self.ask_openai_conversation(self.chain_block))
+        self.block_intermediate_check = self.chain_block[-1]['content']
 
     '''
     ************************
@@ -75,18 +81,18 @@ class Automator:
                   'content': 'Can any elements in the given UI block complete the task directly? \n' +
                              'UI block: ' + str(target_block) +
                              '\nAnswer [Yes] with the target element if any or [No] if not'}
-        self.conversation = [{'role': 'system', 'content': self.role}]
-        self.conversation.append(prompt)
-        self.conversation.append(self.ask_openai_conversation())
-        self.element_complete = self.conversation[-1]['content']
+        self.chain_element = [{'role': 'system', 'content': self.role}]
+        self.chain_element.append(prompt)
+        self.chain_element.append(self.ask_openai_conversation(self.chain_element))
+        self.element_complete = self.chain_element[-1]['content']
 
     def intermediate_element_check(self):
         prompt = {'role': 'user',
                   'content': 'The task may take multiple steps to complete, is there any UI element in the block likely to jump to the UI that is related to the task? ' +
                              'Answer [Yes] with the most related block if any or [No] if not'}
-        self.conversation.append(prompt)
-        self.conversation.append(self.ask_openai_conversation())
-        self.element_intermediate = self.conversation[-1]['content']
+        self.chain_element.append(prompt)
+        self.chain_element.append(self.ask_openai_conversation(self.chain_element))
+        self.element_intermediate = self.chain_element[-1]['content']
 
     '''
     ******************
@@ -106,8 +112,7 @@ class Automator:
         print('\n*** Answer ***\n', resp['choices'][0].message, '\n')
         return dict(resp['choices'][0].message)
 
-    def ask_openai_conversation(self, conversation=None):
-        if not conversation: conversation = self.conversation
+    def ask_openai_conversation(self, conversation):
         print('*** Asking ***\n', conversation)
         resp = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
@@ -121,17 +126,19 @@ class Automator:
     *** Utilities ***
     *****************
     '''
-    def save_conversation(self, file='data/openai/conversation.json'):
-        json.dump(self.conversation, open(file, 'w', encoding='utf-8'), indent=4)
+    def save_conversation(self):
+        json.dump(self.chain_block, open(self.output_chain_block, 'w', encoding='utf-8'), indent=4)
+        json.dump(self.chain_block, open(self.output_chain_element, 'w', encoding='utf-8'), indent=4)
 
-    def load_conversation(self, file='data/openai/conversation.json'):
-        self.conversation = json.load(open(file, 'r', encoding='utf-8'))
+    def load_conversation(self):
+        self.chain_block = json.load(open(self.output_chain_block, 'r', encoding='utf-8'))
+        self.chain_element = json.load(open(self.output_chain_element, 'r', encoding='utf-8'))
 
-    def save_block_descriptions(self, file='data/openai/blocks.json'):
-        json.dump(self.block_descriptions, open(file, 'w', encoding='utf-8'), indent=4)
+    def save_block_descriptions(self):
+        json.dump(self.block_descriptions, open(self.output_block_desc, 'w'), indent=4)
 
-    def load_block_descriptions(self, file='data/openai/blocks.json'):
-        self.block_descriptions = json.load(open(file, 'r', encoding='utf-8'))
+    def load_block_descriptions(self):
+        self.block_descriptions = json.load(open(self.output_block_desc, 'r', encoding='utf-8'))
 
     def show_target_element(self, ele_id):
         print(self.gui.elements_leaves[ele_id])
