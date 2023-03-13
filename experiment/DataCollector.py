c4b6e70da6f8ee899891e2473e600781f7b5d76e
MulongXie <dsh15325@163.com>
2023-03-13 17:10:51 +1100

    fix bug; record acitons

--- experiment/DataCollector.py
+++ experiment/DataCollector.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import time
 import cv2
@@ -7,19 +8,20 @@ import json
 
 
 class DataCollector:
-    def __init__(self, adb_device, app_name='twitter', test_case_no=1, gui_no=0, output_file_root='datacollect'):
+    def __init__(self, adb_device, app_name='twitter', test_case_no=1, ui_no=0, task='Opem Twitter', output_file_root='datacollect'):
         self.adb_device = adb_device  # ppadb device
         self.device_name = self.adb_device.get_serial_no()
 
         self.app_name = app_name
         self.test_case_no = test_case_no
-        self.ui_no = gui_no
+        self.ui_no = ui_no
 
         self.screenshot = None  # cv2 image
         self.vh = None          # dict
 
+        self.task = task        # string, the task description of the testcase
         self.action = {'type': '', 'coordinate': [(-1, -1), (-1, -1)]}  # {'type': 'click' or 'swipe', 'coordinate': [(start), (end)]}
-        self.actions = []       # list of actions for this test case
+        self.record = {'screensize': self.adb_device.wm_size(), 'task':self.task, 'testcase-no':self.test_case_no, 'step-no':self.ui_no, 'actions':[]}
 
         # output file paths
         self.testcase_save_dir = pjoin(output_file_root, app_name, 'testcase' + str(test_case_no))
@@ -28,7 +30,13 @@ class DataCollector:
         self.output_file_path_screenshot = pjoin(self.testcase_save_dir, str(self.ui_no) + '.png')
         self.output_file_path_xml = pjoin(self.testcase_save_dir, str(self.ui_no) + '.xml')
         self.output_file_path_json = pjoin(self.testcase_save_dir, str(self.ui_no) + '.json')
+        self.output_file_path_actions = pjoin(self.testcase_save_dir, 'actions.json')
 
+    '''
+    ************************
+    *** Get raw GUI info ***
+    ************************
+    '''
     def get_devices_info(self):
         print("Device Name:%s Resolution:%s" % (self.device_name, self.adb_device.wm_size()))
 
@@ -143,7 +151,7 @@ class DataCollector:
                     self.adb_device.input_swipe(x_start, y_start, x_device, y_device, 500)
                     # record action
                     self.action['type'] = 'swipe'
-                    self.action['coordinate'][0] = (x_device, y_device)
+                    self.action['coordinate'][1] = (x_device, y_device)
                 # click
                 else:
                     print('\n****** Click (%d, %d) ******' % (x_start, y_start))
@@ -151,10 +159,12 @@ class DataCollector:
                     # record action
                     self.action['type'] = 'click'
                     self.action['coordinate'][1] = (-1, -1)
-                self.actions.append(self.action)
+                self.record['actions'].append(copy.deepcopy(self.action))
+
                 # next ui
-                time.sleep(wait_loading_time)
                 self.ui_no += 1
+                self.save_actions()
+                time.sleep(wait_loading_time)
                 self.cap_ui_info()
                 params[0] = cv2.resize(self.screenshot.copy(), window_size)
                 cv2.imshow(win_name, params[0])
@@ -168,10 +178,16 @@ class DataCollector:
             cv2.destroyWindow(win_name)
             return
 
+    def save_actions(self):
+        self.record['step-no'] = self.ui_no
+        json.dump(self.record, open(self.output_file_path_actions, 'w'), indent=4)
+        print('*** Record action save to %s ***' % self.output_file_path_actions)
+        print(self.record)
+
 
 if __name__ == '__main__':
     from ppadb.client import Client as AdbClient
     client = AdbClient(host="127.0.0.1", port=5037)
 
-    data = DataCollector(client.devices()[0], app_name='twitter', test_case_no=1, gui_no=0, output_file_root='datacollect')
+    data = DataCollector(client.devices()[0], app_name='twitter', test_case_no=1, ui_no=0, output_file_root='datacollect')
     data.record_action(wait_loading_time=1)
