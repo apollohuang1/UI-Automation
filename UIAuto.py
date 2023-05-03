2096b9b94df6f528f39e59758b4cd303c0b5c9d4
MulongXie <dsh15325@163.com>
2023-05-03 17:08:14 +1000

    wrap up aichain with ui block

--- UIAuto.py
+++ UIAuto.py
@@ -23,6 +23,26 @@ class UIAuto:
         self.ans_target_block = None
         self.ans_target_element = None
 
+    '''
+    *******************************************
+    *** AI Chain Checking Elements Directly ***
+    *******************************************
+    '''
+    # def identify_task_target_element(self, task):
+    #     self.conversation += [{'role': 'user', 'content': 'This is a view hierarchy of a UI, can you segment it into functional blocks? Summarize all of its elements and functionalities, and print the "id" of each block.'},
+    #                           {'role': 'user', 'content': str(self.gui.element_tree)},
+    #                           {'role': 'user', 'content': 'To complete the task "' + task + '", which block is the most related one to interact with?'}]
+
+    '''
+    ******************************
+    *** AI Chain with UI Block ***
+    ******************************
+    '''
+    def ai_chain_block(self, task, printlog=False):
+        self.summarize_block(printlog)
+        self.identify_task_target_block(task, printlog)
+        self.identify_task_target_element_from_block(self.get_target_block_node(), printlog)
+
     def summarize_block(self, printlog=False):
         print('------ Summarize GUI Blocks ------')
         self.conversation += [{'role': 'user', 'content': 'This is a view hierarchy of a UI, can you segment it into functional blocks? Summarize all of its elements and functionalities, and print the "id" of each block.'},
@@ -31,6 +51,7 @@ class UIAuto:
         self.conversation.append(self.ans_block_sum)
 
     def identify_task_target_block(self, task, printlog=False):
+        print('------ Identify Target Blocks ------')
         self.task = task
         block_sum = self.conversation[-1]    # {'role':'system', 'content': 'Block sums'}
         conv = [{'role': 'system', 'content': self.role},
@@ -46,6 +67,7 @@ class UIAuto:
         return self.gui.get_ui_element_node_by_id(block_id)
 
     def identify_task_target_element_from_block(self, block_node, printlog=False):
+        print('------ Identify Target Element ------')
         self.conversation += [{'role': 'user', 'content': 'All the elements view hierarchy in the blocks is:'},
                               {'role': 'user', 'content': str(block_node)},
                               {'role': 'user', 'content': 'Which element exactly in this Block is the most related one to interact with to the task "' + self.task + '"? Give the detail information from the view hierarchy.'}]
